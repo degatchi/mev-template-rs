893ca7698d615206351e047c0d5f81ced8026f54
DeGatchi <DeGatchi@users.noreply.github.com>
2022-10-06 00:10:21 +1100

    add: wss + dex data structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use mev_template::config::Config;
+use mev_template::{
+    addresses::{address, SPOOKY_SWAP_FACTORY, SPOOKY_SWAP_ROUTER},
+    config::Config,
+};
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
     let config = Config::new().await;
+
+    let spooky_factory = address(SPOOKY_SWAP_FACTORY);
+    let spooky_router = address(SPOOKY_SWAP_ROUTER);
+    
+    let dex = config.create_dex(spooky_factory, spooky_router).await;
+    dex.get_pairs().await;
 }
