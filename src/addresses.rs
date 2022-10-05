80db754ba7574c86105da06615e24933819355b9
lucus.eth <lucusra@protonmail.com>
2022-10-05 17:08:10 +1100

    add: binding creator

--- src/addresses.rs
+++ src/addresses.rs
@@ -34,3 +34,16 @@ pub async fn setup_signer(
 
     SignerMiddleware::new(provider, wallet)
 }
+
+/// Creates a binding for an ABI.
+/// Example: bind("Example", "src/abi/example.json");
+pub fn bind(name: &str, abi: &str) {
+    let name: String = format!("b_{}", name);
+    let bindings = Abigen::new(&name, abi).unwrap().generate().unwrap();
+    let path: String = format!("src/bindings/{}.rs", name);
+    match std::fs::File::create(path.clone()) {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+    bindings.write_to_file(&path).unwrap();
+}
